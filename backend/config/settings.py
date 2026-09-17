e6e3ff41acdd3f76539370d4c8e1a8eed25bc7f3
R.Arun prasath <arunravi1810@gmail.com>
2026-09-17 18:14:36 +0530

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -15,7 +15,10 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-ems-developmen
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+if os.environ.get('RENDER_EXTERNAL_HOSTNAME'):
+    ALLOWED_HOSTS.append(os.environ.get('RENDER_EXTERNAL_HOSTNAME'))
 
 # Application definition
 INSTALLED_APPS = [
